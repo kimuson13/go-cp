9e5427ddc203f58587f6e81b587c362c6f111864
kimuson <kimuson@jxpress.net>
2021-12-28 20:43:18 +0900

    create base copy package model

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
+
+	"github.com/kimuson13/go-cp/copy"
 )
 
 var (
 	b = flag.Bool("b", false, "make a backup of each existing destination file")
-	i = flag.Bool("i", false, "prompt before overwrite")
 	f = flag.Bool("f", false, "force overwrite")
+	i = flag.Bool("i", false, "prompt before overwrite")
 	r = flag.Bool("r", false, "copy directory")
 )
 
 func main() {
 	flag.Parse()
+	args := flag.Args()
+	if err := copy.Run(*b, *f, *i, *r, args); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
