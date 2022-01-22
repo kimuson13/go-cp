3f76cc1cfbe726ee20cbbb585a9970a42a5c4a48
kimuson <kimuson@jxpress.net>
2022-01-22 18:20:10 +0900

    reset

--- copy/copy.go
+++ copy/copy.go
@@ -2,13 +2,8 @@ package copy
 
 import (
 	"github.com/kimuson13/go-cp/utils"
-	"github.com/kimuson13/go-cp/validate"
 )
 
 func Run(b, f, i, r bool, args []string, std utils.Std) error {
-	if err := validate.Args(args, f, i, std); err != nil {
-		return err
-	}
-
 	return nil
 }
