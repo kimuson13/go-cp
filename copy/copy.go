9b7ba90a65116a34f7b7e79e4492cc0a6af010bb
kimuson <kimuson@jxpress.net>
2022-01-24 11:37:20 +0900

    コメントアウトして消していなかったものの削除

--- copy/copy.go
+++ copy/copy.go
@@ -65,36 +65,3 @@ func MakeCopy(fileName, pasteDir string) error {
 
 	return nil
 }
-
-// func Exists(path string) bool {
-// 	_, err := os.Stat(path)
-
-// 	return !os.IsNotExist(err)
-// }
-
-// func ExistSameFileInDir(path string, copyFiles []string) error {
-// 	files, err := os.ReadDir(path)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	for _, file := range files {
-// 		if !file.IsDir() {
-// 			if ExistFileName(copyFiles, file.Name()) {
-// 				return ErrAlreadyExist
-// 			}
-// 		}
-// 	}
-
-// 	return nil
-// }
-
-// func ExistFileName(target []string, name string) bool {
-// 	for _, v := range target {
-// 		if v == name {
-// 			return true
-// 		}
-// 	}
-
-// 	return false
-// }
